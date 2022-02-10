d3018f80f4f8e79f114eba1de6fe8e585c026214
zed.wu <zed.wu@wayz.ai>
2022-02-10 10:43:59 +0800

    style: 调整文字

--- src/layouts/components/SideNav.tsx
+++ src/layouts/components/SideNav.tsx
@@ -160,7 +160,7 @@ export default defineComponent({
               ),
             operations: () => (
               <span class="version-container">
-                {!this.collapsed && 'TDesign Starter'} {pgk.version}
+                {!this.collapsed && 'Indoor Position'} {pgk.version}
               </span>
             ),
           }}
